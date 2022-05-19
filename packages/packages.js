af160386b49dce904b29849c9a311f1a80daa58c
Joshuarox100 <12418573+Joshuarox100@users.noreply.github.com>
2022-05-18 19:08:53 -0500

    Added repo for VPM.

---
+++ packages/packages.js
@@ -0,0 +1,43 @@
+var packages = {
+  "name": "Joshuarox100's VPM Packages",
+  "author": "vrchat@joshuarox100.com",
+  "packages": {
+    "com.joshuarox100.bmblibraries": {
+      "versions": {
+        "1.0.0": {
+		  "name": "com.joshuarox100.bmblibraries",
+		  "displayName": "BMB Libraries",
+		  "version": "1.0.0",
+		  "unity": "2019.4",
+		  "description": "Common Library",
+		  "gitDependencies": {},
+		  "url": "https://github.com/Joshuarox100/BMB-Libraries.git#1.0.0",
+		  "legacyFolders": {
+			"Assets\\BMB Libraries": ""
+		  }
+        }
+      }
+    },
+    "com.joshuarox100.inventoryinventor": {
+      "versions": {
+        "1.3.3": {
+          "name": "com.joshuarox100.inventoryinventor",
+		  "displayName": "Inventory Inventor",
+		  "version": "1.3.3",
+		  "unity": "2019.4",
+		  "description": "Make inventories fast with Inventory Inventor! With it, you can create inventories with plenty of toggles, all managed by a single Expression Parameter!",
+		  "gitDependencies": {
+			 "com.vrchat.base" : "https://github.com/vrchat/com.vrchat.base.git",
+			 "com.vrchat.avatars" : "https://github.com/vrchat/com.vrchat.avatars.git",
+			 "com.joshuarox100.bmblibraries": "https://github.com/Joshuarox100/BMB-Libraries.git"
+		  },
+		  "url": "https://github.com/Joshuarox100/VRC-Inventory-Inventor.git#v1.3.3",
+		  "legacyFolders": {
+			"Assets\\Inventory Inventor": "",
+			"Assets\\Gizmos\\InventoryInventor": ""
+		  }
+		}
+	  }
+	}
+  }
+}
